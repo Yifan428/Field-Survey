f0a1e4a1b32ff42981564de7f777b275fd40c4f9
Yifan428 <526220305@qq.com>
2017-07-21 15:50:12 +0800

    Commit to GitHub

---
+++ FieldSurvey/FieldSurvey/Model/FieldSurveyJSONParser.swift
@@ -0,0 +1,15 @@
+//
+//  FieldSurveyJSONParser.swfit.swift
+//  FieldSurvey
+//
+//  Created by Shiyifan on 21/07/2017.
+//  Copyright © 2017 Shiyifan. All rights reserved.
+//
+
+import Foundation
+
+class FieldSurveyJSONParser{
+    
+    func parse(_ date: Data)
+    
+}
